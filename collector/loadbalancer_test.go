f23c38aa21d632d32dd0c949f7fddcb1afeb0ac6
febc.yamamoto <yamamoto.febc@gmail.com>
2019-08-02 16:28:36 +0900

    collector tests: LoadBalancer

--- collector/loadbalancer_test.go
+++ collector/loadbalancer_test.go
@@ -307,6 +307,95 @@ func TestLoadBalancerCollector_Collect(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "status and monitor API return error",
+			in: &dummyLoadBalancerClient{
+				find: []*iaas.LoadBalancer{
+					{
+						ZoneName: "is1a",
+						LoadBalancer: &sacloud.LoadBalancer{
+							ID:             101,
+							Name:           "loadbalancer",
+							Tags:           types.Tags{"tag1", "tag2"},
+							Description:    "desc",
+							PlanID:         types.LoadBalancerPlans.Premium,
+							VRID:           1,
+							IPAddresses:    []string{"192.168.0.11", "192.168.0.12"},
+							DefaultRoute:   "192.168.0.1",
+							NetworkMaskLen: 24,
+							InstanceStatus: types.ServerInstanceStatuses.Up,
+							VirtualIPAddresses: []*sacloud.LoadBalancerVirtualIPAddress{
+								{
+									VirtualIPAddress: "192.168.0.101",
+									Port:             80,
+									DelayLoop:        100,
+									SorryServer:      "192.168.0.21",
+									Description:      "vip-desc",
+									Servers: []*sacloud.LoadBalancerServer{
+										{
+											IPAddress:               "192.168.0.201",
+											Port:                    80,
+											Enabled:                 true,
+											HealthCheckProtocol:     types.Protocols.HTTP,
+											HealthCheckResponseCode: http.StatusOK,
+											HealthCheckPath:         "/index.html",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				statusErr:  errors.New("dummy1"),
+				monitorErr: errors.New("dummy2"),
+			},
+			wantMetrics: []*collectedMetric{
+				{
+					desc: c.Up,
+					metric: createGaugeMetric(1, map[string]string{
+						"id":   "101",
+						"name": "loadbalancer",
+						"zone": "is1a",
+					}),
+				},
+				{
+					desc: c.LoadBalancerInfo,
+					metric: createGaugeMetric(1, map[string]string{
+						"id":          "101",
+						"name":        "loadbalancer",
+						"zone":        "is1a",
+						"plan":        "highspec",
+						"ha":          "1",
+						"vrid":        "1",
+						"ipaddress1":  "192.168.0.11",
+						"ipaddress2":  "192.168.0.12",
+						"gateway":     "192.168.0.1",
+						"nw_mask_len": "24",
+						"tags":        ",tag1,tag2,",
+						"description": "desc",
+					}),
+				},
+				{
+					desc: c.VIPInfo,
+					metric: createGaugeMetric(1, map[string]string{
+						"id":           "101",
+						"name":         "loadbalancer",
+						"zone":         "is1a",
+						"vip_index":    "0",
+						"vip":          "192.168.0.101",
+						"port":         "80",
+						"interval":     "100",
+						"sorry_server": "192.168.0.21",
+						"description":  "vip-desc",
+					}),
+				},
+			},
+			wantLogs: []string{
+				`level=warn msg="can't fetch loadbalancer's status: ID: 101" err=dummy1`,
+				`level=warn msg="can't get loadbalancer's NIC metrics: ID=101" err=dummy2`,
+			},
+			wantErrCounter: 2,
+		},
 	}
 
 	for _, tc := range cases {
