cd6b5b84ab7485266efabfe400ebaf339476ad6a
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-01-11 15:26:13 +0900

    Bump to v0.17.0

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ import (
 
 var (
 	// Version of sakuracloud_exporter.
-	Version = "0.16.0"
+	Version = "0.17.0"
 	// Revision or Commit this binary was built from.
 	Revision string
 	// GoVersion running this binary.
