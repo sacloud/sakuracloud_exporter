2b917074ed7b0e3d29471f6333e1ca75374a3e1d
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2023-03-30 14:45:21 +0900

    v0.18.3

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ import (
 
 var (
 	// Version of sakuracloud_exporter.
-	Version = "0.18.2"
+	Version = "0.18.3"
 	// Revision or Commit this binary was built from.
 	Revision string
 	// GoVersion running this binary.
