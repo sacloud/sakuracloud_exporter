388a595821806d855acb48db6931a61d51da587c
hekki <contact@hekki.info>
2024-11-28 17:43:38 +0900

    v0.18.6

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ import (
 
 var (
 	// Version of sakuracloud_exporter.
-	Version = "0.18.5"
+	Version = "0.18.6"
 	// Revision or Commit this binary was built from.
 	Revision string
 	// GoVersion running this binary.
